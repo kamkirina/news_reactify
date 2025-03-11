636b7b7a2c0407506f31957cb236b84c83c3a684
Irina <kamkina2312@gmail.com>
2025-03-11 16:50:11 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/pages/Main/Main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Main />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NewsList/NewsList.jsx
@@ -0,0 +1,12 @@
+import { NewsItem } from "../NewsItem/NewsItem";
+import styles from "./styles.module.css";
+
+export function NewsList({ news }) {
+  return (
+    <ul className={styles.list}>
+      {news.map((item) => (
+        <NewsItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
