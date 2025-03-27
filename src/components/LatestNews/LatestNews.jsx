20bf04d3fb50045ae767b800a7e93a9b9d57a0e5
Irina <kamkina2312@gmail.com>
2025-03-27 14:09:02 +0300

    refactoring LatestNews and add paginationWrapper

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,10 +1,14 @@
 import styles from "./styles.module.css";
 import BannersList from "../BannersList/BannersList";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { getLatestNews } from "../../api/newsApi";
 
-export function LatestNews({banners, isLoading}) {
+export function LatestNews() {
+  const { data, isLoading } = useFetch(getLatestNews);
+
   return (
     <section className={styles.section}>
-      <BannersList banners={banners} isLoading={isLoading}/>
+      <BannersList banners={data && data.news} isLoading={isLoading} />
     </section>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,36 +1,12 @@
 import styles from "./styles.module.css";
-import { getNews } from "../../api/newsApi";
-import { useDebounce } from "../../helpers/hooks/useDebounce";
-import { PAGE_SIZE } from "../../constants/constants";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { useFilters } from "../../helpers/hooks/useFilters";
 import { LatestNews } from "../../components/LatestNews/LatestNews";
 import { NewsByFilters } from "../../components/NewsByFilters/NewsByFilters";
 
 export function Main() {
-  const { filter, changeFilter } = useFilters({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
-
-  const debouncedKeywords = useDebounce(filter.keywords, 1500);
-
-  const { data, isLoading } = useFetch(getNews, {
-    ...filter,
-    keywords: debouncedKeywords,
-  });
-
   return (
     <main className={styles.main}>
-      <LatestNews isLoading={isLoading} banners={data && data.news} />
-      <NewsByFilters
-        news={data?.news}
-        isLoading={isLoading}
-        filter={filter}
-        changeFilter={changeFilter}
-      />
+      <LatestNews />
+      <NewsByFilters />
     </main>
   );
 }
